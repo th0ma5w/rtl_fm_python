3c7884049700c9f1a9ab49e576b68c3b674828a0
thomas <thomas@show.(none)>
2014-02-02 15:55:59 -0500

    Added -fPIC Fixes #1

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-gcc -I /usr/include/libusb-1.0 -I ./convenience/ -I ./getopt -shared -Wl,-soname,rtl_fm_python -o rtl_fm_python.so rtl_fm_python.c convenience/convenience.c getopt/getopt.c -lrtlsdr
+gcc -I /usr/include/libusb-1.0 -I ./convenience/ -I ./getopt -shared -Wl,-soname,rtl_fm_python -o rtl_fm_python.so -fPIC rtl_fm_python.c convenience/convenience.c getopt/getopt.c -lrtlsdr
